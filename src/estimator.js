421575dcbd8b9984eeca24f29d395f87cd05d7d1
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-04-09 04:51:50 +0100

    challange 1 and 2

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    let currentlyInfected = data.reportedCases * 10
+    let severeInfection = data.reportedCases * 50
+
+    let infectionsByRequestedTime = currentlyInfected * (2 ** (timeToElapse/3))
+    let infectionsByRequestedTimeSevere = severeInfection * (2 ** (timeToElapse/3))
+
+    let severeCasesByRequestedTime = (15/100) * infectionsByRequestedTime
+    let severeCasesByRequestedTimeSevere = (15/100) * infectionsByRequestedTimeSevere
+
+    let hospitalBedsByRequestedTime = (35/100) * data.totalHospitalBeds
+
+
+    let impact = {currentlyInfected, infectionsByRequestedTime, hospitalBedsByRequestedTime}
+    let severeImpact = { currentlyinfected: severeInfection, infectionsByRequestedTime: infectionsByRequestedTimeSevere,
+        hospitalBedsByRequestedTime, 
+    }
+};
 
 export default covid19ImpactEstimator;
