121f79b2edd206ebe864bd982169d3b3c39c951d
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-04-09 06:54:12 +0100

    feat:
    make estimate for different time frame

--- src/estimator.js
+++ src/estimator.js
@@ -46,8 +46,6 @@ const covid19ImpactEstimator = (data) => {
   };
   const severeImpact = {
     currentlyinfected: severeInfection,
-    infectionsByRequestedTimeInWeeks: infectionsByRequestedTimeSevereInWeeks,
-    infectionsByRequestedTimeInMonths: infectionsByRequestedTimeSevereInMonths,
     infectionsByRequestedTime: infectionsByRequestedTimeSevere,
     hospitalBedsByRequestedTime: availableHospitalBeds - severeCasesByRequestedTimeSevere,
     severeCasesByRequestedTime: severeCasesByRequestedTimeSevere,
@@ -59,8 +57,111 @@ const covid19ImpactEstimator = (data) => {
     data, impact, severeImpact
   };
   return output;
-};
 
+};
+const weekEstimate = ()=>{
+  const currentlyInfected = data.reportedCases * 10;
+  const severeInfection = data.reportedCases * 50;
+
+
+  const infectionsByRequestedTime = currentlyInfected * (2 ** ((data.timeToElapse / 7) / 3));
+  const infectionsByRequestedTimeSevere = severeInfection
+  * (2 ** ((data.timeToElapse / 7) / 3));
+
+  const severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime;
+  const severeCasesByRequestedTimeSevere = (15 / 100) * infectionsByRequestedTimeSevere;
+
+  const availableHospitalBeds = (35 / 100) * data.totalHospitalBeds;
+
+  const casesForICUByRequestedTime = (5 / 100) * infectionsByRequestedTime;
+  const casesForICUByRequestedTimeSevere = (5 / 100) * infectionsByRequestedTimeSevere;
+
+  const casesForVentilatorsByRequestedTime = (2 / 100) * infectionsByRequestedTime;
+  const casesForVentilatorsByRequestedTimeSevere = (2 / 100) * infectionsByRequestedTimeSevere;
+
+  const dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomePopulation)
+  * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+  const dollarsInFlightSevere = (infectionsByRequestedTimeSevere
+    * data.region.avgDailyIncomePopulation)
+    * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+
+
+  const impact = {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    hospitalBedsByRequestedTime: availableHospitalBeds - severeCasesByRequestedTime,
+    severeCasesByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+  const severeImpact = {
+    currentlyinfected: severeInfection,
+    infectionsByRequestedTime: infectionsByRequestedTimeSevere,
+    hospitalBedsByRequestedTime: availableHospitalBeds - severeCasesByRequestedTimeSevere,
+    severeCasesByRequestedTime: severeCasesByRequestedTimeSevere,
+    casesForICUByRequestedTime: casesForICUByRequestedTimeSevere,
+    casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevere,
+    dollarsInFlight: dollarsInFlightSevere
+  };
+  const output = {
+    data, impact, severeImpact
+  };
+  return output;
+}
+
+  const monthlyEstimator = ()=>{
+    const currentlyInfected = data.reportedCases * 10;
+    const severeInfection = data.reportedCases * 50;
+  
+    const infectionsByRequestedTime = currentlyInfected
+    * (2 ** ((data.timeToElapse / 30) / 3));
+    const infectionsByRequestedTimeSevere = severeInfection
+    * (2 ** ((data.timeToElapse / 30) / 3));
+  
+  
+    const severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime;
+    const severeCasesByRequestedTimeSevere = (15 / 100) * infectionsByRequestedTimeSevere;
+  
+    const availableHospitalBeds = (35 / 100) * data.totalHospitalBeds;
+  
+    const casesForICUByRequestedTime = (5 / 100) * infectionsByRequestedTime;
+    const casesForICUByRequestedTimeSevere = (5 / 100) * infectionsByRequestedTimeSevere;
+  
+    const casesForVentilatorsByRequestedTime = (2 / 100) * infectionsByRequestedTime;
+    const casesForVentilatorsByRequestedTimeSevere = (2 / 100) * infectionsByRequestedTimeSevere;
+  
+    const dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomePopulation)
+    * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+    const dollarsInFlightSevere = (infectionsByRequestedTimeSevere
+      * data.region.avgDailyIncomePopulation)
+      * data.region.avgDailyIncomeInUSD * data.timeToElapse;
+  
+  
+    const impact = {
+      currentlyInfected,
+      infectionsByRequestedTime,
+      hospitalBedsByRequestedTime: availableHospitalBeds - severeCasesByRequestedTime,
+      severeCasesByRequestedTime,
+      casesForICUByRequestedTime,
+      casesForVentilatorsByRequestedTime,
+      dollarsInFlight
+    };
+    const severeImpact = {
+      currentlyinfected: severeInfection,
+      infectionsByRequestedTime: infectionsByRequestedTimeSevere,
+      hospitalBedsByRequestedTime: availableHospitalBeds - severeCasesByRequestedTimeSevere,
+      severeCasesByRequestedTime: severeCasesByRequestedTimeSevere,
+      casesForICUByRequestedTime: casesForICUByRequestedTimeSevere,
+      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevere,
+      dollarsInFlight: dollarsInFlightSevere
+    };
+    const output = {
+      data, impact, severeImpact
+    };
+    return output;    
+  }
+  
 /*
 const data = {
     region: {
