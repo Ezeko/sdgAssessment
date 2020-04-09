334547a8933c3d2b8da49f088a536795eb66e7c6
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-04-09 05:03:24 +0100

    feat :
    add challenge three

--- src/estimator.js
+++ src/estimator.js
@@ -10,10 +10,23 @@ const covid19ImpactEstimator = (data) => {
 
     let hospitalBedsByRequestedTime = (35/100) * data.totalHospitalBeds
 
+    let casesForICUByRequestedTime = (5/100) * infectionsByRequestedTime
+    let casesForICUByRequestedTimeSevere = (5/100) * infectionsByRequestedTimeSevere
 
-    let impact = {currentlyInfected, infectionsByRequestedTime, hospitalBedsByRequestedTime}
+    let casesForVentilatorsByRequestedTime = (2/100) * infectionsByRequestedTime
+    let casesForVentilatorsByRequestedTimeSevere = (2/100) * infectionsByRequestedTimeSevere
+
+    let dollarsInFlight = (infectionsByRequestedTime * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * data.timeToElapse
+    let dollarsInFlightSevere = (infectionsByRequestedTimeSevere * data.region.avgDailyIncomePopulation) * data.region.avgDailyIncomeInUSD * data.timeToElapse
+
+
+    let impact = {currentlyInfected, infectionsByRequestedTime, hospitalBedsByRequestedTime, casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime, dollarsInFlight
+    }
     let severeImpact = { currentlyinfected: severeInfection, infectionsByRequestedTime: infectionsByRequestedTimeSevere,
-        hospitalBedsByRequestedTime, 
+        hospitalBedsByRequestedTime, casesForICUByRequestedTime: casesForICUByRequestedTimeSevere, casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTimeSevere,
+        dollarsInFlight: dollarsInFlightSevere
+
     }
 };
 
